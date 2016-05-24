80066b07979eb23122bb545e3a190bb8e53546c0
Ruru Chen <balaja2179@gmail.com>
2016-05-24 20:40:39 +0800

    1. Add main function to read cas file. 2. Modify local variable DIR to global variable.
     Changes to be committed:
    	modified:   change_format.py

--- change_format.py
+++ change_format.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 def make_sure_folder_exists(folder):
     """
@@ -8,9 +9,20 @@ def make_sure_folder_exists(folder):
         os.makedirs(folder)
 
 def set_environment():
-    SCRIPT_DIR = os.getcwd()
-    RESULT_FOLDER = SCRIPT_DIR + "\\result_data"
-    make_sure_folder_exists(RESULT_FOLDER)
+	"""
+	Get current working dir and create a result folder.
+	"""
+	global SCRIPT_DIR, FILE_DIR, RESULT_FOLDER
+	SCRIPT_DIR = os.getcwd()
+	FILE_DIR = SCRIPT_DIR + "\\data\\"
+	RESULT_FOLDER = SCRIPT_DIR + "\\result_data"
+	make_sure_folder_exists(RESULT_FOLDER)
 
+def main():
+	#for a_csv in os.listdir(FILE_DIR):
+	for a_csv in ['2014_01.csv', '2014_02.csv']:
+		data = pd.read_csv(FILE_DIR + a_csv)
+		print data.head()
 
 set_environment()
+main()
