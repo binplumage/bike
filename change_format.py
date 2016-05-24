c1d1c550f6b832d144a431c8ba4f107534443fff
Ruru Chen <balaja2179@gmail.com>
2016-05-24 22:29:21 +0800

    1. Add function time_to_sec compute the hh:mm:ss to second.
    Changes to be committed:
    	modified:   change_format.py

--- change_format.py
+++ change_format.py
@@ -18,11 +18,29 @@ def set_environment():
 	RESULT_FOLDER = SCRIPT_DIR + "\\result_data"
 	make_sure_folder_exists(RESULT_FOLDER)
 
+def time_to_sce(hms):
+	"""
+	00:00:00 to 0 sec.
+	"""
+	sec = 3600 * int(hms[11:13]) + 60 * int(hms[14:16]) + int(hms[17:])
+	return sec
+
 def main():
 	#for a_csv in os.listdir(FILE_DIR):
 	for a_csv in ['2014_01.csv', '2014_02.csv']:
-		data = pd.read_csv(FILE_DIR + a_csv)
-		print data.head()
+		df = pd.read_csv(FILE_DIR + a_csv)
+		data = df.as_matrix()
+		data_number = data.shape[0]
+		print data_number
+		print "tipduration start_time stop_time start_station_id stop_station_id"
+
+		for line in range(15):
+			tipduration = data[line][0]
+			start_time = time_to_sce(data[line][1])
+			stop_time= time_to_sce(data[line][2])
+			start_station_id = data[line][3]
+			stop_station_id = data[line][7]
+			
 
 set_environment()
 main()
